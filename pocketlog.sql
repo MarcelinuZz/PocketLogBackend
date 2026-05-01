99413e56b0d1375532dafcb32f9a3399c466465e
Marcelinus Wijaya <wijayaoeymarcelinus@gmail.com>
2026-05-01 19:12:00 +0700

    mengupdate database untuk reminder fitur nanti

--- pocketlog.sql
+++ pocketlog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Apr 2026 pada 10.18
+-- Waktu pembuatan: 01 Bulan Mei 2026 pada 14.11
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -33,6 +33,34 @@ CREATE TABLE `categories` (
   `name` varchar(100) NOT NULL,
   `icon_url` varchar(255) DEFAULT NULL,
   `color_hex` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `categories`
+--
+
+INSERT INTO `categories` (`id`, `user_id`, `name`, `icon_url`, `color_hex`) VALUES
+('04105732-8a8d-494a-b11a-4fb7494e71dd', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'gaming', 'icon ngasal', 'color'),
+('469d9642-afc1-460a-8258-5fa550e3cf86', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'playing', 'icon ngasal', 'color'),
+('50260480-b34f-4d6e-9596-7f1c1e721f41', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'clubing', 'icon ngasal', 'color'),
+('7f2b3e4a-9c1d-4b8f-a2e5-6d7c8b9a0f1e', NULL, 'Transfer', 'ic_swap_horiz', '#9E9E9E'),
+('a67e6058-5321-46ae-be5a-5ccfab80fd73', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'education', 'icon ngasal', 'color'),
+('f176b232-3a59-4cd3-b2b8-ab528a3839a7', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'Job', 'icon ngasal', 'color'),
+('fdc48b42-e8b2-4eba-89b6-6b6cb59a5368', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'sport', 'icon ngasal', 'color');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notification_history`
+--
+
+CREATE TABLE `notification_history` (
+  `id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  `reminder_id` varchar(36) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,6 +97,23 @@ CREATE TABLE `register_otps` (
   `email` varchar(255) NOT NULL,
   `otp_code` varchar(6) NOT NULL,
   `expires_at` datetime NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reminders`
+--
+
+CREATE TABLE `reminders` (
+  `id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `time_scheduled` time NOT NULL,
+  `days_active` varchar(255) NOT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -135,6 +180,20 @@ CREATE TABLE `transactions` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `user_id`, `type`, `amount`, `category_id`, `from_wallet_id`, `to_wallet_id`, `transaction_date`, `title`, `note`, `receipt_image_url`, `created_at`) VALUES
+('3a89244d-68a7-4def-9a8b-40fa28d82509', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'income', 450000.00, '50260480-b34f-4d6e-9596-7f1c1e721f41', NULL, '747e7c56-daf0-4d5e-b666-f45ec10268af', '2026-04-30 02:00:00', 'Gak tauV3', 'Gaji bulan AprilV3', NULL, '2026-04-30 12:32:40'),
+('580259dc-e638-49d1-b803-82ec6ecbf216', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'income', 400000.00, 'f176b232-3a59-4cd3-b2b8-ab528a3839a7', NULL, '6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', '2026-04-30 02:00:00', 'Gak tauV2', 'Gaji bulan AprilV2', NULL, '2026-04-30 12:30:43'),
+('6dca286f-d74e-4cfa-92f6-a5556f776c76', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'income', 250000.00, '50260480-b34f-4d6e-9596-7f1c1e721f41', NULL, '6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', '2026-04-30 02:00:00', 'Gak tauV3', 'Gaji bulan AprilV3', NULL, '2026-04-30 12:32:06'),
+('84d290e7-f2a2-4135-95f4-76733a190be9', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'income', 300000.00, 'f176b232-3a59-4cd3-b2b8-ab528a3839a7', NULL, '747e7c56-daf0-4d5e-b666-f45ec10268af', '2026-04-30 02:00:00', 'Gak tau', 'Gaji bulan April', NULL, '2026-04-30 12:29:06'),
+('ae340b63-a119-47b2-9aa2-dc14c8b38e19', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'expense', 75000.00, '469d9642-afc1-460a-8258-5fa550e3cf86', '6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', NULL, '2026-04-30 06:00:00', 'Beli Kopi Spesial', 'Revisi: Ternyata kopinya lebih mahal', NULL, '2026-04-30 12:28:48'),
+('cacdda8b-1253-4102-a1fa-96290ba7067d', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'expense', 2000000.00, '50260480-b34f-4d6e-9596-7f1c1e721f41', '747e7c56-daf0-4d5e-b666-f45ec10268af', NULL, '2026-04-30 02:00:00', 'ngurangin', 'Gaji bulan AprilV3', NULL, '2026-04-30 12:33:45'),
+('cdd1baee-368a-462a-a4fc-9cf25ae16f02', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'transfer', 250000.00, '7f2b3e4a-9c1d-4b8f-a2e5-6d7c8b9a0f1e', '6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', '747e7c56-daf0-4d5e-b666-f45ec10268af', '2026-04-30 09:00:00', 'Revisi Transfer tabungan', 'Nambahin 50rb', NULL, '2026-04-30 12:36:18'),
+('d605a003-4b64-4a18-8d20-9a7a37567c31', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'income', 1500000.00, 'f176b232-3a59-4cd3-b2b8-ab528a3839a7', NULL, '747e7c56-daf0-4d5e-b666-f45ec10268af', '2026-04-30 02:00:00', 'Gaji Bulanan', 'Gaji bulan April', NULL, '2026-04-30 12:28:05');
+
 -- --------------------------------------------------------
 
 --
@@ -160,11 +219,11 @@ INSERT INTO `users` (`id`, `name`, `gender`, `DOB`, `email`, `avatar_url`, `crea
 ('2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'Linus', 'Male', '2001-04-14', 'wijayaoeymarcelinus@gmail.com', '', '2026-04-26 14:45:17'),
 ('6fa45bfc-1bd6-4656-8cc6-a115e039f5ff', 'Albert', 'Male', '2006-04-02', 'wilsonalbert@gmail.com', '', '2026-04-02 16:50:01'),
 ('77501367-c570-4af8-930a-96b798f4feaa', 'OwenZ', 'Female', '2004-07-11', 'Owennias@gmail.com', 'https://airbrush.com/image-enhancer', '2026-04-13 10:38:29'),
-('8e5f9caa-6e46-4a37-87e8-e061eee56ec6', 'Maruseru Rinusu', NULL, NULL, 'rinusu123@gmail.com', 'https://lh3.googleusercontent.com/a/ACg8ocKuVLTNr1CVNNPrPGHoxBgGn2YkMvH52auWqxpGUf9llNQApA=s96-c', '2026-04-13 10:50:53'),
+('81c58e6c-2327-4c42-82d0-cbacef945ad5', 'rinusu', 'Male', '2001-07-21', 'rinusu123@gmail.com', '', '2026-05-01 10:46:11'),
+('ab2cb1f2-bd9a-4721-910c-be632d14f64d', 'Marcelinus Wijaya', NULL, NULL, 'wijayaoeymarcelinus2006@gmail.com', 'https://lh3.googleusercontent.com/a/ACg8ocJvvo8l9DKVZBOhBizXhkfd4E8xSBUSZSsa9hSqdFPIGLTaLQ=s96-c', '2026-05-01 10:52:09'),
 ('ac9dde04-1570-4d1d-882a-d69a74624a14', 'Oweniass', 'Male', '2003-07-04', 'Owenniass@gmail.com', '', '2026-04-13 13:14:13'),
 ('cb3735de-5ce2-40b2-94de-cfad422ae928', 'Owenzz', 'Female', '2004-07-21', 'Owennia@gmail.com', 'https://airbrush.com/image-enhance', '2026-04-03 16:04:05'),
-('ce20641b-e427-49a4-9f67-ef4afa9f572a', 'Albertus', 'Male', '2006-07-02', 'wilsonalbertus@gmail.com', '', '2026-04-02 16:51:16'),
-('e87a2f37-0ff7-4ce2-b492-d077a8b1eaf5', 'Linus2006', 'Male', '2001-04-14', 'wijayaoeymarcelinus2006@gmail.com', '', '2026-04-28 05:43:09');
+('ce20641b-e427-49a4-9f67-ef4afa9f572a', 'Albertus', 'Male', '2006-07-02', 'wilsonalbertus@gmail.com', '', '2026-04-02 16:51:16');
 
 -- --------------------------------------------------------
 
@@ -185,20 +244,7 @@ CREATE TABLE `user_identities` (
 
 INSERT INTO `user_identities` (`id`, `user_id`, `provider`, `provider_id`) VALUES
 ('0aa189a0-a918-46d3-9a98-806e88ab2d67', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'google', '101357322244149981937'),
-('166398c5-7202-4656-9b7c-ab5f6dfa2493', '8e5f9caa-6e46-4a37-87e8-e061eee56ec6', 'google', '108553418755461759779');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user_notification`
---
-
-CREATE TABLE `user_notification` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(36) DEFAULT NULL,
-  `category_id` varchar(36) DEFAULT NULL,
-  `Message` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+('ae818db8-d397-482b-b180-7113533a888b', 'ab2cb1f2-bd9a-4721-910c-be632d14f64d', 'google', '118223078270721532988');
 
 -- --------------------------------------------------------
 
@@ -219,9 +265,9 @@ INSERT INTO `user_passwords` (`user_id`, `hashed_password`) VALUES
 ('0c982661-a29b-4667-b02d-2326eab4973d', '$2b$11$DGQ67DYvLJTrcO.AVjYrbupZSsDFsXmPdRUkrJjarIjloHLdEplxG'),
 ('2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', '$2b$11$PjnFpP7SgOegRPG0IvGQgO/Bzys007IqXwpcGq6aGkMVs.eo/bi.C'),
 ('77501367-c570-4af8-930a-96b798f4feaa', '$2b$11$Okw/vaR1SYTw5GLn7gIwvOOWkPGccxPkhqteABKMytwmJgb6J0UL6'),
+('81c58e6c-2327-4c42-82d0-cbacef945ad5', '$2b$11$AplR6QRYWX/.P7MObtoYAuQKvlqIrakl879Ib20zRzEBXOVPRAB7y'),
 ('ac9dde04-1570-4d1d-882a-d69a74624a14', '$2b$11$XWBOF.zTm3fWSIG/jLMCLePnxilpG3bTvG4R6HxdxolIVECaGM5NK'),
-('cb3735de-5ce2-40b2-94de-cfad422ae928', '$2b$11$nmc0DnZ2rAAptKvVVs95Du6L/BKZGhxFgw/VqYj9HkmuCDkbHQe2i'),
-('e87a2f37-0ff7-4ce2-b492-d077a8b1eaf5', '$2b$11$XPVNdzT8WA2a6hC.RZJI0ukLwbX1nF5Nvo6MQR6KwdQg/QIQY.RoW');
+('cb3735de-5ce2-40b2-94de-cfad422ae928', '$2b$11$nmc0DnZ2rAAptKvVVs95Du6L/BKZGhxFgw/VqYj9HkmuCDkbHQe2i');
 
 -- --------------------------------------------------------
 
@@ -235,6 +281,14 @@ CREATE TABLE `user_settings` (
   `appearance` varchar(20) DEFAULT 'Light',
   `language` varchar(20) DEFAULT 'English'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user_settings`
+--
+
+INSERT INTO `user_settings` (`user_id`, `currency`, `appearance`, `language`) VALUES
+('81c58e6c-2327-4c42-82d0-cbacef945ad5', 'IDR', 'dark', 'indo'),
+('ab2cb1f2-bd9a-4721-910c-be632d14f64d', 'IDR', 'Light', 'English');
 
 -- --------------------------------------------------------
 
@@ -257,8 +311,8 @@ CREATE TABLE `wallets` (
 --
 
 INSERT INTO `wallets` (`id`, `user_id`, `name`, `account_number`, `balance`, `color_hex`, `created_at`) VALUES
-('6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'BLU', '2355543645', 0.00, '#fcba03', '2026-04-30 06:54:28'),
-('747e7c56-daf0-4d5e-b666-f45ec10268af', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'BCA', '325367346', 0.00, '#fcba03', '2026-04-30 07:32:42'),
+('6fcc255a-2d0c-4bf5-bd8e-d981c94418c8', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'BLU', '2355543645', 325000.00, '#fcba03', '2026-04-30 06:54:28'),
+('747e7c56-daf0-4d5e-b666-f45ec10268af', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'BCA', '325367346', 500000.00, '#fcba03', '2026-04-30 07:32:42'),
 ('9e923da5-9f99-4559-b457-16ccb676cd69', '2b8b49a7-f2c7-446e-9e4c-48d0a6c30619', 'BBA', '123870192831', 0.00, '#j33aa', '2026-04-30 06:53:40');
 
 --
@@ -273,6 +327,14 @@ ALTER TABLE `categories`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indeks untuk tabel `notification_history`
+--
+ALTER TABLE `notification_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `reminder_id` (`reminder_id`);
+
+--
 -- Indeks untuk tabel `otp_verifications`
 --
 ALTER TABLE `otp_verifications`
@@ -284,6 +346,13 @@ ALTER TABLE `otp_verifications`
 --
 ALTER TABLE `register_otps`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `reminders`
+--
+ALTER TABLE `reminders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indeks untuk tabel `token_blacklist`
@@ -315,14 +384,6 @@ ALTER TABLE `users`
 ALTER TABLE `user_identities`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
-
---
--- Indeks untuk tabel `user_notification`
---
-ALTER TABLE `user_notification`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indeks untuk tabel `user_passwords`
@@ -357,19 +418,13 @@ ALTER TABLE `otp_verifications`
 -- AUTO_INCREMENT untuk tabel `register_otps`
 --
 ALTER TABLE `register_otps`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `token_blacklist`
 --
 ALTER TABLE `token_blacklist`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- AUTO_INCREMENT untuk tabel `user_notification`
---
-ALTER TABLE `user_notification`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -382,10 +437,23 @@ ALTER TABLE `categories`
   ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Ketidakleluasaan untuk tabel `notification_history`
+--
+ALTER TABLE `notification_history`
+  ADD CONSTRAINT `notification_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `notification_history_ibfk_2` FOREIGN KEY (`reminder_id`) REFERENCES `reminders` (`id`) ON DELETE SET NULL;
+
+--
 -- Ketidakleluasaan untuk tabel `otp_verifications`
 --
 ALTER TABLE `otp_verifications`
   ADD CONSTRAINT `otp_verifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `reminders`
+--
+ALTER TABLE `reminders`
+  ADD CONSTRAINT `reminders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `transactions`
@@ -401,13 +469,6 @@ ALTER TABLE `transactions`
 --
 ALTER TABLE `user_identities`
   ADD CONSTRAINT `user_identities_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `user_notification`
---
-ALTER TABLE `user_notification`
-  ADD CONSTRAINT `user_notification_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_notification_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `user_passwords`
