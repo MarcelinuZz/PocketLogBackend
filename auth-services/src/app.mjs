e7eb8ba889d0045ea1b750afc4a509efa8ababdc
Marcelinus Wijaya <wijayaoeymarcelinus@gmail.com>
2026-05-31 21:44:25 +0700

    penambahan cors dan juga perbaikan bug oleh docker

--- auth-services/src/app.mjs
+++ auth-services/src/app.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import express from 'express';
+import cors from 'cors';
 import passport from 'passport';
 import authRoutes from "./routes/authRoutes.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
@@ -13,6 +14,11 @@ import passportGoogleStrategy from './middleware/passportGoogleStrategy.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
+
+app.use(cors({
+    origin: process.env.FLUTTER_APP_URL || "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- gateway-services/src/app.mjs
+++ gateway-services/src/app.mjs
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { setupProxies } from './routes/proxyRoutes.mjs';
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.FLUTTER_APP_URL || "http://localhost:5173",
+    credentials: true
+}));
 
 setupProxies(app);
 
